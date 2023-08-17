956c5e71743fd4f3e21bfadf73e6974b4f0f0428
grmoshkevich <grmoshkevich@gmail.com>
2023-08-17 16:22:40 +0400

    skip types for now

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,13 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   try {
     const response = await fetch('http://localhost:3000/posts/for-user', {
